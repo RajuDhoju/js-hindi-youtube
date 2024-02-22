5677f5e624788c3316df5b48d96d3df607ea8e57
RajuDhoju <160472618+RajuDhoju@users.noreply.github.com>
2024-02-22 12:41:54 +0545

    Add files via upload

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+ 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3)    //code readability should be high 
+
+
+console.log("Raju") 
+
+let name = "raju"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to power 53
+// bigint may be used in trading where more numbers are used
+// string => ""
+// boolean => true/false
+// null => standalone value  for eg: for defining value of temperature, zero(0) is temp. but due to damaged meter we need null value
+// undefined =>      not assigned value like let state;
+// symbol => unique
+
+// object
+
+console.log(typeof null);   // object
+console.log(typeof undefined);   // undefined
